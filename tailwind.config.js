554bdbd63a699226838fafdf3ee0be113420635b
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-03-21 21:48:14 +0900

    chore: tailwindcssを追加

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // どの階層にあるファイルに適用するか
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
