df67ef4f42ddafcf49e0d4cc1b4436e12aff3f33
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-03-27 10:07:32 +0900

    feat: クエリを追加

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -10,26 +10,26 @@ export const GET_USERS = gql`
   }
 `
 
-export const GET_USERS_LOCAL = gql`
-  query GetUsers {
-    users(order_by: { created_at: desc }) @client {
-      id
-      name
-      created_at
-    }
-  }
-`
+// export const GET_USERS_LOCAL = gql`
+//   query GetUsers {
+//     users(order_by: { created_at: desc }) @client {
+//       id
+//       name
+//       created_at
+//     }
+//   }
+// `
 
 export const GET_USER_IDS = gql`
-  query GetUsers {
+  query GetUserIds {
     users(order_by: { created_at: desc }) {
       id
     }
   }
 `
 export const GET_USER_BY_ID = gql`
-  query GetUsers {
-    users(order_by: { created_at: desc }) {
+  query GetUserById($id: uuid!) {
+    users_by_pk(id: $id) {
       id
       name
       created_at
