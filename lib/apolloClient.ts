79a65259f5993fcfe68b0a82702cca4bbd58f750
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-04-16 21:28:36 +0900

    chore: エンドポイントを追加

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -6,11 +6,11 @@ let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 
 const createApolloClient = () => {
   return new ApolloClient({
+    // ブラウザではない場合、サーバーサイドで処理されている場合
     ssrMode: typeof window ="undefined",
     link: new HttpLink({
-      uri:''
+      uri:'https://t-lesson-hasura.hasura.app/v1/graphql'
     })
-
     cache: new InMemoryCache()
   })
 }
