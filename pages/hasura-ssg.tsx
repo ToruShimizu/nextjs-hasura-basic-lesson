e28aa103c21693a147be83a05e1257b1a2747ed7
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-04-17 14:37:19 +0900

    feat: SSGで取得するページを追加

---
+++ pages/hasura-ssg.tsx
@@ -0,0 +1,44 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
+import { initializeApollo } from '../lib/apolloClient'
+import { GET_USERS } from '../graphql/queries'
+import { GetUsersQuery, Users } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+interface Props {
+  users: ({
+    __typename?: 'users'
+  } & Pick<Users, 'id' | 'name' | 'created_at'>)[]
+}
+const HasuraSSG: VFC<Props> = ({ users }) => {
+  return (
+    <Layout title="Hasura SSG">
+      <p className="mb-3 font-bold">SSG+ISR</p>
+      {users?.map((user) => {
+        return (
+          <Link key={user.id} href={`/users/${user.id}`}>
+            <a className="my-1 cursor-pointer" data-testid={`link-${user.id}`}>
+              {user.name}
+            </a>
+          </Link>
+        )
+      })}
+    </Layout>
+  )
+}
+export default HasuraSSG
+
+// アプリのビルド時にサーバーサイドで実行される処理
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<GetUsersQuery>({
+    query: GET_USERS,
+  })
+  return {
+    // FCのpropsに返す
+    props: { users: data.users },
+    // ISRの有効化
+    revalidate: 1,
+  }
+}
